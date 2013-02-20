a995b4e53088c8f1b154f727ffc442130cd54098
mattkrebs <matt@44lights.net>
2013-02-20 14:41:23 -0600

    First Commit

---
+++ WriteCongress.Web/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using WriteCongress.Web.Models;
+
+namespace WriteCongress.Web
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "419608328133774",
+                appSecret: "0bb1ac7de8e955cc8a3dfaf31a6c6c53");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
