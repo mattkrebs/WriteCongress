b8c7b57cae978bd3f1eacfa62201cebb154f35d9
George Hilal <george.hilal@gmail.com>
2013-02-23 16:36:08 -0600

    added prices page

--- WriteCongress.Web/Controllers/HomeController.cs
+++ WriteCongress.Web/Controllers/HomeController.cs
@@ -8,25 +8,15 @@ namespace WriteCongress.Web.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Content/
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+    
     }
 }
