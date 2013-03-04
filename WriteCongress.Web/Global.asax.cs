3b4ebd6e4b7a52b55832dd351a1c18beedeb72c7
Joseph Spaur <joseph@trypaper.com>
2013-03-03 21:14:48 -0800

    getting rid of jquery ui and begining of auth

--- WriteCongress.Web/Global.asax.cs
+++ WriteCongress.Web/Global.asax.cs
@@ -22,7 +22,6 @@ namespace WriteCongress.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
