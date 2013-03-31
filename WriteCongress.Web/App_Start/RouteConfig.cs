224918bdcfe552c3c10746c1fd77dcfdf3fe5314
Joseph Spaur <jhspaur@hotmail.com>
2013-03-30 23:57:53 -0700

    FAQ, contact us page, etc.
    also deleted a bunch of old images

--- WriteCongress.Web/App_Start/RouteConfig.cs
+++ WriteCongress.Web/App_Start/RouteConfig.cs
@@ -14,14 +14,14 @@ namespace WriteCongress.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute("Issue", "Issues/{slug}", new { controller = "Issues", action = "Details" });
+            routes.MapRoute("IssueLetter", "Issues/{issueSlug}/{letterSlug}", new { controller = "Issues", action = "IssueLetter" });
 
             routes.MapRoute("About", "about", new { controller = "Home", action = "About" });
             routes.MapRoute("Contact", "contact-us", new { controller = "Home", action = "Contact" });
             routes.MapRoute("SendUsLetters", "send-us-letters", new { controller = "Home", action = "SendUsLetters" });
             routes.MapRoute("HowThisWorks", "how-this-works", new { controller = "Home", action = "HowThisWorks" });
-
-            routes.MapRoute("IssueLetter", "Issues/{issueSlug}/{letterSlug}", new { controller = "Issues", action = "IssueLetter" });
             routes.MapRoute("FAQ", "FAQ", new { controller = "Home", Action = "FAQ" });
+            
             routes.MapRoute(name: "Default", url: "{controller}/{action}/{id}", defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
 
         }
