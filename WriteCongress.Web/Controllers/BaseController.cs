4b98c3b9870f420f70c0d1bad5b11940d78167a2
Joseph Spaur <jhspaur@hotmail.com>
2013-04-04 11:38:06 -0700

    redirect to SSL and WWW

--- WriteCongress.Web/Controllers/BaseController.cs
+++ WriteCongress.Web/Controllers/BaseController.cs
@@ -11,9 +11,12 @@ namespace WriteCongress.Web.Controllers
     public class BaseController : Controller
     {
         private WriteCongressConnection _db;
-        public WriteCongress.Core.WriteCongressConnection Db {
-            get {
-                if (_db == null) {
+        public WriteCongress.Core.WriteCongressConnection Db
+        {
+            get
+            {
+                if (_db == null)
+                {
                     _db = new WriteCongressConnection();
                 }
                 return _db;
@@ -23,6 +26,29 @@ namespace WriteCongress.Web.Controllers
         public User AuthenticatedUser { get; private set; }
 
         protected override void OnAuthorization(AuthorizationContext filterContext) {
+            var request = filterContext.RequestContext.HttpContext.Request;
+            
+            //redirect to https://wwww. as needed
+            if (!request.IsLocal) {
+                bool canonicalizeRedirect = false;
+                string host = request.Url.Host;
+                if (!host.StartsWith("www.", StringComparison.OrdinalIgnoreCase)) {
+                    canonicalizeRedirect = true;
+                    host = "www." + host;
+                }
+
+                if (!request.IsSecureConnection) {
+                    canonicalizeRedirect = true;
+                }
+
+                if (canonicalizeRedirect) {
+                    string url = String.Format("https://{0}{1}", host, request.RawUrl);
+                    filterContext.Result = new RedirectResult(url);
+                    return;
+                }
+            }
+
+
             if (User.Identity.IsAuthenticated) {
                 var user = Db.Users.FirstOrDefault(u => u.SessionId == User.Identity.Name);
                 if (user != null) {
