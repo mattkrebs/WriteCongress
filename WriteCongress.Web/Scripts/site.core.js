5a89e57999ca7b9d41ce8bd9d96d10a4457c732d
Joseph Spaur <jhspaur@hotmail.com>
2013-06-20 14:57:56 -0700

    continued migration to KO

--- WriteCongress.Web/Scripts/site.core.js
+++ WriteCongress.Web/Scripts/site.core.js
@@ -1,39 +1,6 @@
 ﻿/// <reference path="site.Geolocator.js" />
 /// <reference path="knockout-2.2.1.debug.js" />
 /// <reference path="site.CongressPersonFinder.js" />
-var setSection = function (selector, html) {
-    var section = $(selector);
-    section.html(html);
-    $('img', section).tooltip({ placement: 'bottom' });
-};
-
-function setMySenators(data) {
-    var html = '';
-    if (data == null) {
-        return;
-    }
-    if (data.length != 2) {
-        html = '<img class="img-polaroid" title="Your zipcode alone isn\'t enough to determine your Senators. Try entering your full address on a letter." src="https://writecongress.blob.core.windows.net/congress-photos/unknown.jpg"/>';
-    } else {
-        html = '<img class="img-polaroid" title="' + data[0].FullNameAndTitle + '" src="https://writecongress.blob.core.windows.net/congress-photos/' + data[0].OpenCongressId + '-50px.jpg"/>';
-        html += '<img class="img-polaroid" title="' + data[1].FullNameAndTitle + '" src="https://writecongress.blob.core.windows.net/congress-photos/' + data[1].OpenCongressId + '-50px.jpg"/>';
-    }
-    setSection('#mysenators', html);
-}
-
-function setMyRep(data) {
-    var html = '';
-    if (data === "needaddress") {
-        html = '<img class="img-polaroid" title="Your zipcode alone isn\'t enough to determine your Represenative. Try entering your full address on a letter." src="https://writecongress.blob.core.windows.net/congress-photos/unknown.jpg"/>';
-    } else if (data == null) {
-        html = '';
-    } else {
-        html = '<img class="img-polaroid" title="' + data.FullNameAndTitle + '" src=""/>';
-    }
-    setSection('#myrep', html);
-}
-
-
 $(function () {
     var CongressPerson = function (fullNameAndTitle, openCongressId) {
         var self = this;
@@ -66,30 +33,40 @@ $(function () {
             $.get('/Data/SenatorsByState', { state: self.State() }).done(function(data) {
                 if (data.length == 2) {
                     self.Senators([new CongressPerson(data[0].FullNameAndTitle, data[0].OpenCongressId), new CongressPerson(data[1].FullNameAndTitle, data[1].OpenCongressId)]);
-                    self.CongressionalDistrict(-1);
                 }
             });
+            self.Save();
         });
-        //self.CongressionalDistrict.subscribe(function (district) {
-        //    if ((district !== -1 && district !== null) && self.State() != null) {
-        //        $.get('/Data/RepresentativeByStateAndDistrict', { state: self.State(), district: district }, function (data) {
-        //            if (data.length == 0) {
-        //                self.Representative(data[0]);
-        //            } else {
-        //                self.Representative(emptyPerson);
-        //            }
-        //        });
-        //    } else {
-        //        self.Representative(emptyPerson);                
-        //    }
-        //});
+
+        self.CongressionalDistrict.subscribe(function (district) {
+            //console.log('changing district to ' + district);finan
+            if ((district !== -1 && district !== null) && self.State() != null) {
+                $.get('/Data/RepresentativeByStateAndDistrict', { state: self.State(), district: district }, function (data) {
+                    if (data.length == 1) {
+                        self.Representative(new CongressPerson(data[0].FullNameAndTitle,data[0].OpenCongressId));
+                    } else {
+                        self.Representative(emptyPerson);
+                    }
+                });
+            } else {
+                self.Representative(emptyPerson);                
+            }
+            self.Save();
+        });
+
+        this.Save = function () {
+            window.localStorage.setItem("myCongressionalDistrict", ko.toJSON(self));
+        }
     };
 
     myCongressionalDistrict = new CongressPersonDisplayViewModel(null, -1);
-    var district = window.localStorage.getItem("congressionalDistrict");
+    var district = window.localStorage.getItem("myCongressionalDistrict");
     if (district != null) {
         var info = JSON.parse(district);
-        myCongressionalDistrict = new CongressPersonDisplayViewModel(info.State, info.District);
+        myCongressionalDistrict = new CongressPersonDisplayViewModel(info.State, info.CongressionalDistrict);
+        myCongressionalDistrict.State(info.State);
+        myCongressionalDistrict.CongressionalDistrict(info.CongressionalDistrict);
+        myCongressionalDistrict.Senators(info.Senators);
     }
     ko.applyBindings(myCongressionalDistrict, document.getElementById('mydistrict'));
 });
