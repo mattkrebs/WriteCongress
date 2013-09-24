896f37373fc916206e2f16fe2764e05507e1ee4c
George Hilal <george.hilal@gmail.com>
2013-09-24 00:38:48 -0500

    start adding validation

--- WriteCongress.Web/Models/Account.cs
+++ WriteCongress.Web/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,17 +8,55 @@ namespace WriteCongress.Web.Models
 {
     public class Account
     {
+        public Account(WriteCongress.Core.User user)
+        {
+            if (user != null)
+            {
+                this.FirstName = user.FirstName;
+                this.LastName = user.LastName;
+                this.AddressOne = user.AddressOne;
+                this.AddressTwo = user.AddressTwo;
+                this.City = user.City;
+                this.State = user.State;
+                this.ZipCode = user.ZipCode;
+                this.Email = user.Email;
+                this.Identity = user.Identity;
+
+
+
+            }
+
+        }
+
+
         public int? Id { get; set; }
         public string Identity { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string AddressLineOne { get; set; }
-        public string AddressLineTwo { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
 
+        [MaxLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Address line one cannot be longer than 50 characters")]
+        public string AddressOne { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Address line two cannot be longer than 50 characters")]
+        public string AddressTwo { get; set; }
+
+        [MaxLength(50, ErrorMessage = "City cannot be longer than 50 characters")]
+        public string City { get; set; }
+
+        [MaxLength(2, ErrorMessage = "Postal code cannot be longer than 10 characters")]
+        public string State { get; set; }
+
+        [MaxLength(10, ErrorMessage = "Postal code cannot be longer than 10 characters")]
+        public string ZipCode { get; set; }
+
+        [MaxLength(80, ErrorMessage = "Email code cannot be longer than 10 characters")]
         public string Email { get; set; }
         public string Password { get; set; }
     }
+
+
 }
