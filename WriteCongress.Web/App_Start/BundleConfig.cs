262e9ddd5df058e2d78f272f063786f56466ddf9
George Hilal <george.hilal@gmail.com>
2013-12-17 23:38:45 -0600

    convert templates continue cleanup

--- WriteCongress.Web/App_Start/BundleConfig.cs
+++ WriteCongress.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,28 @@ namespace WriteCongress.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new StyleBundle("~/css/site").Include("~/content/site.css").Include("~/content/letter.css"));
+           bundles.Add(new StyleBundle("~/css/site").Include("~/content/site.css").Include("~/content/letter.css"));
+
+           //bundles.Add(new StyleBundle("~/theme").Include("~//Theme/css/bootstrap.css").Include("~/content/letter.css"));
+
+           StyleBundle themeBundle = new StyleBundle("~/css/theme");
+
+           themeBundle.Include("~/Theme/css/bootstrap.css");
+           themeBundle.Include("~/Theme/css/animations.css");
+           themeBundle.Include("~/Theme/css/superfish.css");
+           themeBundle.Include("~/Theme/css/revolution-slider/css/settings.css");
+           themeBundle.Include("~/Theme/css/team-member.css");
+
+           themeBundle.Include("~/Theme/css/prettyPhoto.css");
+           themeBundle.Include("~/Theme/css/nivo-slider.css");
+           themeBundle.Include("~/Theme/css/style.css");
+           themeBundle.Include("~/Theme/css/colors/green.css");
+           themeBundle.Include("~/Theme/css/theme-responsive.css");
+           
+           bundles.Add(themeBundle);
+
+
+            //bundles.Add(new StyleBundle("~/css/site").Include("~/content/css/Site.css").Include("~/content/letter.css"));
             bundles.Add(new StyleBundle("~/css/print").Include( new string[] {"~/content/print.css"})); 
             bundles.Add(new ScriptBundle("~/js/site").Include("~/scripts/site.*"));
             bundles.Add(new ScriptBundle("~/js/plugins").Include("~/scripts/plugins/jquery.*"));
